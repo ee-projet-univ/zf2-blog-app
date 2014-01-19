6298bdf4930fe2a8ec81ed51f07ba17f4f7fc081
papjul <julien.papasian@gmail.com>
2014-01-19 14:58:24 +0100

    Use provided user views instead

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -64,34 +64,6 @@ return array(
                     ),
                 ),
             ),
-            'register' => array(
-                'type' => 'segment',
-                'options' => array(
-                    'route' => '/app/register[/:action]',
-                    'constraints' => array(
-                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                    ),
-                    'defaults' => array(
-                        '__NAMESPACE__' => 'Application\Controller',
-                        'controller'    => 'Register',
-                        'action'        => 'index',
-                    ),
-                ),
-            ),
-            'login' => array(
-                'type' => 'segment',
-                'options' => array(
-                    'route' => '/app/login[/:action]',
-                    'constraints' => array(
-                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                    ),
-                    'defaults' => array(
-                        '__NAMESPACE__' => 'Application\Controller',
-                        'controller'    => 'Login',
-                        'action'        => 'index',
-                    ),
-                ),
-            ),
         ),
     ),
     'service_manager' => array(
@@ -117,9 +89,6 @@ return array(
         'invokables' => array(
             'Application\Controller\Index' => 'Application\Controller\IndexController',
             'Application\Controller\Search' => 'Application\Controller\SearchController',
-            'Application\Controller\Register' => 'Application\Controller\RegisterController',
-            'Application\Controller\Login' => 'Application\Controller\LoginController',
-            'Application\Controller\Logout' => 'Application\Controller\LogoutController',
         ),
     ),
     'view_manager' => array(
@@ -132,11 +101,6 @@ return array(
             'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
             'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
             'application/search/index' => __DIR__ . '/../view/application/search/index.phtml',
-            'application/register/index' => __DIR__ . '/../view/application/register/index.phtml',
-            'application/register/submit' => __DIR__ . '/../view/application/register/submit.phtml',
-            'application/login/index' => __DIR__ . '/../view/application/login/index.phtml',
-            'application/login/submit' => __DIR__ . '/../view/application/login/submit.phtml',
-            'application/logout/index' => __DIR__ . '/../view/application/logout/index.phtml',
             'error/404'               => __DIR__ . '/../view/error/404.phtml',
             'error/index'             => __DIR__ . '/../view/error/index.phtml',
         ),
